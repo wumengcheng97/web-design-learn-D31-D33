6ad0cfcd032c3d32afd95a44ee29dd5d77caed51
wumengcheng97 <82813735+wumengcheng97@users.noreply.github.com>
2021-05-22 11:53:03 +0800

    Add files via upload

---
+++ checkbox.js
@@ -0,0 +1,58 @@
+function renderCheckBox(CheckBoxObj, data) {
+    var html = "<input name='全选' type='checkbox' value='0' class='check-all'/><lable>全选</lable>";
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        html += "<input name="+element.text+" type='checkbox' value="+element.value+" class='check-item'/><lable>"+element.text+"</lable>";
+    }
+    CheckBoxObj.innerHTML = html;
+
+    CheckBoxObj.addEventListener("click",function(event){
+        let input = event.target;
+        if (input.type == "checkbox") {
+            var parent = input.parentElement;
+            var all = parent.getElementsByClassName("check-all")[0];
+            var chils = parent.getElementsByClassName("check-item");
+            if (input.value == 0) { //全选 做全选对应的逻辑 
+                for (let i = 0; i < chils.length; i++) {
+                    const element = chils[i];
+                    element.checked = true;
+                }
+                console.log(chils);
+            } else {  //做子选项对应的逻辑
+                var num = 0;
+                for (let i = 0; i < chils.length; i++) {
+                    const element = chils[i];
+                    if (element.checked) {
+                        num ++;
+                    }
+                }   
+                if (num == 3) { //全选中
+                    console.log(all);
+                    all.checked = true;
+                }else if (num < 3 && num > 0) {
+                    all.checked = false;
+                }else{
+
+                }
+            }
+            var region_temp = region_obj.querySelectorAll(".check-item");
+            var product_temp = product_obj.querySelectorAll(".check-item");
+            var region = new Array();
+            var product = new Array();
+            for (let i = 0; i < region_temp.length; i++) {
+                const element = region_temp[i];
+                if (element.checked) {
+                    region.push(element.name);
+                }
+            }
+            for (let i = 0; i < product_temp.length; i++) {
+                const element = product_temp[i];
+                if (element.checked) {
+                    product.push(element.name);
+                }
+            }
+            renderTable(document.getElementById("table-wrapper"), selectValue(region, product), region.length, product.length);
+        }
+    });
+   
+}
